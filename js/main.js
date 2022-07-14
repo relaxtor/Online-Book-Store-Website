c5590ca80a1a2a917ee888d940f25764a97e5ebc
Igor Negin <igornegin@yandex.ru>
2022-07-14 21:00:51 +0300

    new working save

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+// Fixed header-bottom
+const headerFix = document.querySelector('[data-header-bottom]');
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 80) {
+        headerFix.classList.add('active');
+    } else {
+        headerFix.classList.remove('active');
+    }
+});
