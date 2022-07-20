ad01de7312d874020b05aafdea72f43619fa52a1
Igor Negin <igornegin@yandex.ru>
2022-07-20 22:10:47 +0300

    create section featured & newsletter

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,52 @@
 // SWIPER HOME BOOK ITEMS
-const swiper = new Swiper(".book-slider", {
-    loop: true,
-    centeredSlide: true,
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+const swiperHome = new Swiper(".book-slider", {
+  loop: true,
+  centeredSlide: true,
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      425: {
-        slidesPerView: 2,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      1024: {
-        slidesPerView: 4,
-      },
+    425: {
+      slidesPerView: 2,
     },
-  });
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// SWIPER FEATURED SLIDE
+const swiperFeatured = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlide: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    425: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
