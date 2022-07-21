78f7dcf09c9e4698bc53df1006899892effdee0a
Igor Negin <igornegin@yandex.ru>
2022-07-21 22:40:31 +0300

    add section arrivals, deal, reviews

--- js/swiper.js
+++ js/swiper.js
@@ -50,3 +50,56 @@ const swiperFeatured = new Swiper(".featured-slider", {
     },
   },
 });
+
+// SWIPER ARRIVALS SLIDE
+const swiperArrivals = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlide: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// SWIPER REVIEWS SLIDE
+const swiperReviews = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  loop: true,
+  grabCursor: true,
+  centeredSlide: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
